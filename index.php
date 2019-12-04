9e21236edead3d26b16b638dae9ec00497813ce8
Rajrahane <rajrahane10399@gmail.com>
2019-12-04 22:16:02 +0530

    fix date conversion,implement temp conversion

--- index.php
+++ index.php
@@ -1,4 +1,103 @@
-<?php include "apiCalls.php";
+<?php
+			//CONSTANTS
+			//error_reporting(0);	
+			//print_r($_REQUEST);		
+            define("GOOGLE_GEOCODING_API","https://maps.googleapis.com/maps/api/geocode/json?");
+			define("FORECAST_IO_API","https://api.forecast.io/forecast/");
+			define("GOOGLE_KEY","AIzaSyDWBtO4XwwiZCwCDr6z2aK8rXZMuO0OTNM");
+			define("DARK_SKY_KEY","d815d6a10ae34088a87b7e07ffdc16ec");
+			$longitude;
+			if(isset($_POST["longitude"]))
+				$longitude=$_POST["longitude"];
+			$latitude;
+			if(isset($_POST["latitude"]))
+				$latitude=$_POST["latitude"];
+			if(isset($_POST["currentLocationCheckBox"])){
+				$isCurrentLocCheckBoxEnabled=$_POST["currentLocationCheckBox"];
+			}
+			else{
+				$isCurrentLocCheckBoxEnabled="false";
+			}
+			$state="";
+			if(isset($_POST["state"])){
+				$state=$_POST["state"];
+			}
+			$street="";
+			if(isset($_POST["street"])){
+				$state=$_POST["street"];
+			}
+			$city="";
+			if(isset($_POST["city"])){
+				$state=$_POST["city"];
+			}
+			if($isCurrentLocCheckBoxEnabled=="false"){
+				//print_r("inside getGeoloc");
+				function getGeoLocation($street,$city,$state){
+					$query=http_build_query([
+						'address' => "[".$street.",".$city.",".$state, 
+						'key' => GOOGLE_KEY,
+					]);
+					$arrContextOptions=array(
+						"ssl" =>array(
+							"verify_peer"=>false,
+							"verify_peer_name"=>false,
+						)
+					);
+					return file_get_contents(GOOGLE_GEOCODING_API.$query,false,
+						stream_context_create($arrContextOptions)
+					);
+				}
+				if($state!="default"){					
+					$locationJSON=json_decode(getGeoLocation($street,$city,$state),true);
+					if(isset($locationJSON["results"][0]))
+					$latitude =$locationJSON["results"][0]["geometry"]["location"]["lat"];
+					if(isset($locationJSON["results"][0]))
+					$longitude=$locationJSON["results"][0]["geometry"]["location"]["lng"];
+				}
+			}
+			
+			if(isset($latitude) && 	isset($longitude)){
+				//print_r("both set");print_r($latitude);print_r($longitude);
+				function getCurrentWeather($latitude,$longitude){
+					$params=DARK_SKY_KEY . "/" . $latitude . "," . $longitude . "?";
+					$query=http_build_query([
+						'exclude' => "minutely,hourly,alerts,flags",	
+					]);	
+					$arrContextOptions=array(
+						"ssl" =>array(
+							"verify_peer"=>false,
+							"verify_peer_name"=>false,	
+						)
+					);
+					return file_get_contents(FORECAST_IO_API.$params.$query,false,
+						stream_context_create($arrContextOptions)				
+					);
+                }			
+                $currentWeatherJSONDoc=json_decode(getCurrentWeather($latitude,$longitude));
+                header('Content-type:application/json');
+                echo json_encode($currentWeatherJSONDoc);
+                die();
+				//$currentWeatherJSONDoc=;		
+				//print_r($currentWeatherJSONDoc);		
+				//echo $currentWeatherJSONDoc;
+			}
+			
+			//print_r("printing");print_r($_REQUEST["currentLocationCheckBox"]);
+			/*if(!isset($_REQUEST["currentLocationCheckBox"]) && $_REQUEST["state"]!="default"){
+				print_r("in here");
+				//print_r($locationJSON);		
+				print_r($_REQUEST["latitude"]);
+				print_r($_REQUEST["longitude"]);				
+			}*/
+			//print_r($_POST);
+			/*if( isset( $longitude ) && isset( $latitude ) ){
+				print_r("decoding");
+				print_r("lat=");
+				print_r($latitude);
+				//$currentWeatherJSON=json_decode(currentWeatherJSONDoc);	
+				print_r("encoding");		
+			}*/
+		
 ?>
 <html>
 	<head>
@@ -49,45 +148,45 @@
                         	Current Location
                     	</label>
                 	</div>
-                	<input type="text" name="latitude" id="latitude" value="-1">
-                	<input type="text" name="longitude" id="longitude" value="-1"><br>
+                	<input type="hidden" name="latitude" id="latitude" value="-1">
+                	<input type="hidden" name="longitude" id="longitude" value="-1"><br>
                 	<button type="submit" class="btn btn-primary" id="searchBtn">Submit</button>
             	</form>
 			</div>
 		</div>
-		<div id="currentTemperatureCard" class="card bg-warning" style="margin:auto;width:40%">
+		<div id="currentTemperatureCard" class="card bg-warning" style="margin:auto;width:40%;display:none" >
             <div class="card-body">
                 <h2 id="currentCityName">Los Angeles</h2>
                 <h6	id="currentTimezone">America/LA</h6>
                 <h1>
                     <label id="currentTemperature">69.54</label>
                     <sup><img src="https://cdn3.iconfinder.com/data/icons/virtual-notebook/16/button_shape_oval-512.png" width="10px" height="10px">
-                    </sup>F
+                    </sup>C
                 </h1>
                 <h3 id="currentSummary">Clear</h3><br>
                 <div class="container">
                         <div class="row">
-                            <div class="col-sm">
+                            <div class="col-sm" title="Humidity">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-16-512.png" width="20px" height="20px">
-                                <br><label id="currentHumidity">0.73</label>
+                                <br><label id="currentHumidity">73</label>%
                             </div>
-                            <div class="col-sm">
+                            <div class="col-sm" title="Pressure">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-25-512.png" width="20px" height="20px">
                                 <br><label id="currentPressure">1010.5</label>
                             </div>
-                            <div class="col-sm">
+                            <div class="col-sm" title="Wind Speed">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-27-512.png" width="20px" height="20px">
                                 <br><label id="currentWindSpeed">2.86</label>
                             </div>
-                            <div class="col-sm">
+                            <div class="col-sm" title="Visibility">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-30-512.png" width="20px" height="20px">
                                 <br><label id="currentVisibility">7.327</label>
                             </div>
-                            <div class="col-sm">
+                            <div class="col-sm" title="Cloud Cover">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-28-512.png" width="20px" height="20px">
                                 <br><label id="currentCloudCover">0.05</label>
                             </div>
-                            <div class="col-sm">
+                            <div class="col-sm" title="Ozone">
                                 <img src="https://cdn2.iconfinder.com/data/icons/weather-74/24/weather-24-512.png" width="20px" height="20px">
                                 <br><label id="currentOzone">287.2</label>
                             </div>
@@ -133,14 +232,23 @@
 							+"&state="+weatherSearchForm.state.value;
 				}
 				console.log(query);
-				xmlHttpRequest.open("post","homePage.php",false);
+				xmlHttpRequest.open("post","index.php",false);
 				xmlHttpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 				xmlHttpRequest.send(query);
 				var currentWeatherJSONDoc=xmlHttpRequest.responseText;
 				console.log(currentWeatherJSONDoc);
 				var currentWeatherJSON=JSON.parse(currentWeatherJSONDoc);
+				document.getElementById("currentTemperatureCard").style.display="block";
 				createCurrentWeatherCard(currentWeatherJSON);
+				clearTemperaturePredictionTable();
 				createDailyWeatherPredictionTable(currentWeatherJSON);
+			}
+			function clearTemperaturePredictionTable(){
+				document.getElementById("searchTable").innerHTML="";
+			}
+			function fahrenheitToCelsius(f){
+				var celsiusValue=(f-32)*5/9;
+				return celsiusValue.toPrecision(2);
 			}
 			function fetchGeoLocation(){
 				var xmlHttpRequest=new XMLHttpRequest();
@@ -188,9 +296,9 @@
 			}
 			function createCurrentWeatherCard(currentWeatherJSON){
 				document.getElementById("currentTimezone").innerHTML=currentWeatherJSON.timezone;
-				document.getElementById("currentTemperature").innerHTML=currentWeatherJSON.currently.temperature;
+				document.getElementById("currentTemperature").innerHTML=fahrenheitToCelsius(currentWeatherJSON.currently.temperature);
 				document.getElementById("currentSummary").innerHTML=currentWeatherJSON.currently.summary;
-				document.getElementById("currentHumidity").innerHTML=currentWeatherJSON.currently.humidity;
+				document.getElementById("currentHumidity").innerHTML=currentWeatherJSON.currently.humidity*100;
 				document.getElementById("currentPressure").innerHTML=currentWeatherJSON.currently.pressure;
 				document.getElementById("currentWindSpeed").innerHTML=currentWeatherJSON.currently.windSpeed;
 				document.getElementById("currentVisibility").innerHTML=currentWeatherJSON.currently.visibility;
@@ -219,12 +327,12 @@
 				if(dailyWeatherPredictionArray.length!=0){
 					var searchTableBody=document.createElement("tbody");
 					for(i in dailyWeatherPredictionArray){
-						var predictionDate=new Date(dailyWeatherPredictionArray[i].time);
+						var predictionDate=new Date(dailyWeatherPredictionArray[i].time*1000);
 						var column1=createColumn(predictionDate.toDateString(),document,"td");
 						var column2=createColumn(dailyWeatherPredictionArray[i].icon,document,"td");
 						var column3=createColumn(dailyWeatherPredictionArray[i].summary,document,"td");
-						var column4=createColumn(dailyWeatherPredictionArray[i].temperatureHigh,document,"td");
-						var column5=createColumn(dailyWeatherPredictionArray[i].temperatureLow,document,"td");
+						var column4=createColumn(fahrenheitToCelsius(dailyWeatherPredictionArray[i].temperatureHigh),document,"td");
+						var column5=createColumn(fahrenheitToCelsius(dailyWeatherPredictionArray[i].temperatureLow),document,"td");
 						var column6=createColumn(dailyWeatherPredictionArray[i].windSpeed,document,"td");
 						var weatherRow=document.createElement("tr");
 						weatherRow.appendChild(column1);weatherRow.appendChild(column2);
